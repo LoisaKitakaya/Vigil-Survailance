5b6f1ea214470507410b925f806fb49d394e2aa0
Freedom Loisa <kitakayaloisa@gmail.com>
2022-06-29 13:31:52 +0300

    created different css files for each page, and created component to change page title

---
+++ src/components/PageTitle.js
@@ -0,0 +1,5 @@
+const PageTitle = (newTitle) => {
+  return (document.title = newTitle);
+};
+
+export default PageTitle;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import PageTitle from "../components/PageTitle";
+
+const Home = () => {
+  PageTitle("VS | Home");
+  
+  return (
+    <div className="home">
+      <h1>Home</h1>
+    </div>
+  );
+};
+
+export default Home;
